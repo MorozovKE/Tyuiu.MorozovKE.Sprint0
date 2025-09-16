f79549a5a4dd972cdad92fd5d7b719343b713568
MorozovKE <krekodill@gmail.com>
2025-09-17 03:57:32 +0500

    Task1

---
+++ Tyuiu.MorozovKE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.MorozovKE.Sprint0.Task1.V0
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!    zxc gool");
+            string name = "Кирилл";
+            int age = 21;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + "!");
+            Console.ReadKey();
+
+        }
+    }
+}
